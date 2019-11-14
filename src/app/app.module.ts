42b4340b986783057c75bc9aa5a35e4e3c0b8115
yrborruel <yrborruel@gmail.com>
2019-11-14 08:43:28 -0300

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
